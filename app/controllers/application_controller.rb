8baa8a22b0af2625893b9373414fea491101273a
Maurius Hill <146677371+mauriushill@users.noreply.github.com>
2024-03-11 20:30:08 +0000

    Authorized photo in photos controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   
-  include Pundit::Authorization
+  include Pundit
 
   before_action :authenticate_user!
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
