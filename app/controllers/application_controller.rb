a4e1576eb2543661425f73d4c0bfed59b4234738
Maurius Hill <146677371+mauriushill@users.noreply.github.com>
2024-03-11 21:03:56 +0000

    Added application policy with some validations.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,10 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+  after_action :verify_authorized, except: :index
+  
+after_action :verify_policy_scoped, only: :index
+
   private
 
     def user_not_authorized
